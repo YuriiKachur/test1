c523f1e017b42205fca8f0f61e3d698c6ad01b87
Yurii Kachur <163346915+YuriiKachur@users.noreply.github.com>
2024-08-15 21:05:48 +0300

    Update1 app.js

--- app.js
+++ app.js
@@ -1,61 +1,31 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const port = process.env.PORT || 3001;
+const port = 3000;
 
-app.get("/", (req, res) => res.type('html').send(html));
+// Пример данных меню
+const menu = [
+  { day: 'Monday', items: ['Soup', 'Salad', 'Bread', 'Apple'] },
+  { day: 'Tuesday', items: ['Pasta', 'Tomato Sauce', 'Cheese', 'Banana'] },
+  { day: 'Wednesday', items: ['Chicken', 'Rice', 'Vegetables', 'Orange'] },
+  { day: 'Thursday', items: ['Fish', 'Potatoes', 'Green Beans', 'Pear'] },
+  { day: 'Friday', items: ['Pizza', 'Carrot Sticks', 'Yogurt', 'Grapes'] }
+];
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// Маршрут для главной страницы
+app.get('/', (req, res) => {
+  let menuHtml = '<h1>Menu of School Canteen</h1><ul>';
+  menu.forEach(dayMenu => {
+    menuHtml += `<li><h2>${dayMenu.day}</h2><ul>`;
+    dayMenu.items.forEach(item => {
+      menuHtml += `<li>${item}</li>`;
+    });
+    menuHtml += '</ul></li>';
+  });
+  menuHtml += '</ul>';
+  res.send(menuHtml);
+});
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
-
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+// Запуск сервера
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
